d555ab689f4f436111ecd7c11d635f02a72b6d9b
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-04-28 16:41:38 -0400

    The user is now able to control the ship by pressing the left and right arrow keys.

--- game_functions.py
+++ game_functions.py
@@ -1,13 +1,19 @@
 import sys
 import pygame
 
+from bullet import Bullet
 
-def check_keydown_events(event, ship):
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         # Move the ship to the right
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # Create a new bullet and add it to the bullets group.
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -19,13 +25,13 @@ def check_keyup_events(event, ship):
             ship.moving_left = False
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
